a727a7c196efb5d4505247e3930ffecac2d9a4c8
Maycon Carlete <maycon.carlete@gmail.com>
2023-07-29 13:21:16 -0400

    stack

--- src/chip8memory.c
+++ src/chip8memory.c
@@ -1,8 +1,18 @@
+#include <assert.h>
+
 #include "chip8memory.h"
 
+
+static void chip8_memory_is_in_bounds(int index){
+    assert(index >= 0 && index < CHIP8_MEMORY_SIZE);
+}
+
 void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char value){
+    chip8_memory_is_in_bounds(index);
     memory->memory[index] = value;
 }
+
 unsigned char chip8_memory_get(struct chip8_memory* memory, int index){
+    chip8_memory_is_in_bounds(index);
     return memory->memory[index];
 }
