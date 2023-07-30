a9a22a416b6a030d634be8ade9e386a131b4c9ea
Maycon Carlete <maycon.carlete@gmail.com>
2023-07-30 10:13:15 -0400

    load chip8 rom

--- include/chip8.h
+++ include/chip8.h
@@ -1,12 +1,15 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include <stddef.h>
+
 #include "config.h"
 #include "chip8memory.h"
 #include "chip8registers.h"
 #include "chip8stack.h"
 #include "chip8keyboard.h"
 #include "chip8screen.h"
+
 
 struct chip8 {
     struct chip8_memory memory;
@@ -17,5 +20,7 @@ struct chip8 {
 };
 
 void chip8_init(struct chip8* chip8);
+void chip8_load(struct chip8* chip8, const char* buf, size_t size);
+void chip8_exec(struct chip8* chip8, unsigned char opcode);
 
 #endif
--- include/chip8memory.h
+++ include/chip8memory.h
@@ -9,5 +9,5 @@ struct chip8_memory {
 
 void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char value);
 unsigned char chip8_memory_get(struct chip8_memory* memory, int index);
-
+unsigned short chip8_memory_get_short(struct chip8_memory* memory, int index);
 #endif 
