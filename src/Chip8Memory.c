bd03294ae04190a97382ff23bd33419680a5b539
Maycon Carlete <maycon.carlete@gmail.com>
2023-08-03 21:56:36 -0400

    feat: add memory index verification through bounds

--- src/Chip8Memory.c
+++ src/Chip8Memory.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 
 #include "Chip8Memory.h"
@@ -10,10 +11,16 @@ void free_chip8_memory(struct Chip8Memory* memory){
     free(memory);
 }
 
+static void memory_in_bounds(int index){
+    assert(index >= 0 && index < CHIP8_MEMORY_SIZE);
+}
+
 unsigned char set_chip8_memory(struct Chip8Memory* memory, int index, unsigned char value){
+    memory_in_bounds(index);
     memory->memory[index] = value;
     return memory->memory[index];
 }
 unsigned char get_chip8_memory(struct Chip8Memory* memory, int index){
+    memory_in_bounds(index);
     return memory->memory[index];
 }   
