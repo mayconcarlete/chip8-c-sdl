381ddfe07e1ab1e4b665207045485b605c72b6a6
Maycon Carlete <maycon.carlete@gmail.com>
2023-08-14 20:16:19 -0400

    tests: chip8 stack

--- test/Chip8StackTest.c
+++ test/Chip8StackTest.c
@@ -7,14 +7,19 @@
 void test_stack_chip8_push(void){
     // arrange
     struct Chip8Stack* stack = create_chip8_stack();
-    stack->stack_pointer_register = 0;
     memset(stack, 0, sizeof(struct Chip8Stack));
-    
+
     // act
     stack_chip8_push(stack, 0x0ff);
-    
+
     // assert
     TEST_CHECK_(stack->stack_pointer_register == 1, "it should increment the stack value");
+    TEST_CHECK_(stack->stack[stack->stack_pointer_register - 1] = 0xff, "it should get the value pointed by the stack register.");
+
+    stack_chip8_pop(stack);
+    // assert
+    TEST_CHECK_(stack->stack[stack->stack_pointer_register] == 0x0ff, "it should return the value of address in position 0 of stack");
+    TEST_CHECK_(stack->stack_pointer_register == 0, "it should point to position 0 after pop.");
 
     // after all
     free_chip8_stack(stack);
